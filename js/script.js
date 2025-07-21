a95e4cfef11c07cbe4e732fc41c07bd6c278decd
Mern2025 <merndeveloper2025@gmail.com>
2025-07-22 03:06:44 +0600

    h1 and h2 modified

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let buttons = document.querySelectorAll('.button')
 let body = document.querySelector('body')
+let h1 = document.querySelector('h1')
+let h2 = document.querySelector('h2')
 
 buttons.forEach((button)=>{
     console.log(button)
@@ -7,18 +9,30 @@ buttons.forEach((button)=>{
         console.log(e.target)
         if(e.target.id === 'grey'){
             body.style.backgroundColor = e.target.id
+            h1.style.color = 'white'
+            h2.style.color = 'white'
         }
           if(e.target.id === 'white'){
             body.style.backgroundColor = e.target.id
+            h1.style.color = 'black'
+            h2.style.color = 'black'
          }
          else if(e.target.id === 'blue'){
              body.style.backgroundColor = e.target.id
+             h1.style.color = 'white'
+              h2.style.color = 'white'
          }
          else if(e.target.id === 'yellow'){
              body.style.backgroundColor = e.target.id
+             h1.style.color = 'red'
+              h2.style.color = 'red'
          }
            else if(e.target.id === 'purple'){
              body.style.backgroundColor = e.target.id
+             h1.style.color = 'white'
+             h2.style.color = 'white'
+            
+             
          }
         
     })
